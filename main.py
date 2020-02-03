be913018e4292a92ff27b45e381fd37488db967b
Makhabane Mipi <makhabane@operadev.com>
2020-02-03 21:30:21 +0200

    requirements file, gitignore and adding the main function in the main file

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ import numpy as np
 import imageio
 import scipy.ndimage
 import matplotlib
+
 matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
 
-def dodge(front,back):
-    result=front*255/(255-back) 
-    result[result>255]=255
-    result[back==255]=255
+
+def dodge(front, back):
+    result = front * 255 / (255 - back)
+    result[result > 255] = 255
+    result[back == 255] = 255
     return result.astype('uint8')
 
+
 def grayscale(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
-img ="http://static.cricinfo.com/db/PICTURES/CMS/263600/263697.20.jpg"
 
-s = imageio.imread(img)
-g=grayscale(s)
-i = 255-g
+if __name__ == '__main__':
+    img = "http://static.cricinfo.com/db/PICTURES/CMS/263600/263697.20.jpg"
 
-b = scipy.ndimage.filters.gaussian_filter(i,sigma=10)
-r= dodge(b,g)
+    s = imageio.imread(img)
+    g = grayscale(s)
+    i = 255 - g
 
-plt.imshow(r, cmap="gray")
+    b = scipy.ndimage.filters.gaussian_filter(i, sigma=10)
+    r = dodge(b, g)
 
-plt.savefig('~/to.png', img)   # save the figure to file
-plt.close(r)   
+    plt.imshow(r, cmap="gray")
+
+    plt.savefig('~/to.png', img)  # save the figure to file
+    plt.close(r)
